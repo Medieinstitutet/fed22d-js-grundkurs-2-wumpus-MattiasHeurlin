da56c25046cb37c9a7ba29e2f4c16cf680464f67
Mattias heurlin <mattias.heurlin@hotmail.com>
2022-12-06 11:20:01 +0100

    Syntax update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import './style/style.scss';
 
-const mainStage: unknown = document.querySelector('#mainStage');
-const mainTextArea: unknown = document.querySelector('#mainTextArea');
-const userTextInput: unknown = document.querySelector('#userTextInput');
+const mainStage: object = document.querySelector('#mainStage');
+const mainTextArea: object = document.querySelector('#mainTextArea');
+const userTextInput: object = document.querySelector('#userTextInput');
 document.querySelector('#startBtn')?.addEventListener('click', startGame);
 let userName = '';
 console.log(userTextInput);
@@ -11,71 +11,83 @@ let wumpusCurrentLocation: any;
 
 interface CaveRooms {
   wumpusIsHere: boolean;
-  containsItem: string;
+  containsItem: Array<string>;
+  containsTrap: boolean;
+  containsBat: boolean;
 }
 
 let allCaves: CaveRooms = [
-  [{wumpusIsHere: false,
-  containsItem: ''}, {wumpusIsHere: false,
-    containsItem: ''}],
-  [{wumpusIsHere: false,
-    containsItem: ''}, {wumpusIsHere: false,
-      containsItem: ''}],
-  [{wumpusIsHere: false,
-    containsItem: ''}, {wumpusIsHere: false,
-    containsItem: ''}],
-  [{wumpusIsHere: false,
-    containsItem: ''}, {wumpusIsHere: false,
-      containsItem: ''}],
-  [{wumpusIsHere: false,
-    containsItem: ''}, {wumpusIsHere: false,
-      containsItem: ''}],
-  [{wumpusIsHere: false,
-    containsItem: ''}, {wumpusIsHere: false,
-      containsItem: ''}],
-  [{wumpusIsHere: false,
-    containsItem: ''}, {wumpusIsHere: false,
-    containsItem: ''}],
-  [{wumpusIsHere: false,
-    containsItem: ''}, {wumpusIsHere: false,
-    containsItem: ''} ],
-  [{wumpusIsHere: false,
-    containsItem: ''}, {wumpusIsHere: false,
-      containsItem: ''}],
-  [{wumpusIsHere: false,
-    containsItem: ''}, {wumpusIsHere: false,
-      containsItem: ''}
-]];
+  [
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+  ],
+  [
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+  ],
+  [
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+  ],
+  [
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+  ],
+  [
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+  ],
+  [
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+  ],
+  [
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+  ],
+  [
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+  ],
+  [
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+  ],
+  [
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+    { wumpusIsHere: false, containsItem: [], containsTrap: false, containsBat: false },
+  ],
+];
 
-
-function getRandomInt(max): any {
+function getRandomInt(max:number): number {
   return Math.floor(Math.random() * max);
 }
 
 function placeWumpus(): void {
-  let random1: int = getRandomInt(4);
-  let random2: int = getRandomInt(3);
-  allCaves[random1][random2].wumpusIsHere = true;
+  let random1: number = getRandomInt(4);
+  let random2: number = getRandomInt(3);
+  allCaves[random1, random2].wumpusIsHere = true;
   wumpusCurrentLocation = `Wumpus location: ${random1}, ${random2}`;
+  console.log(wumpusCurrentLocation);
+}
+function placeTraps(): void {
+  for (let i = 0; i < 5;i++) {
+    allCaves[getRandomInt(4), getRandomInt(3)].containsTrap = true;
+    console.log('Trap has been placed ' + i)
+  }
+}
+function placeBats(): void {
+  for (let i = 0; i < 5;i++) {
+    allCaves[getRandomInt(4), getRandomInt(3)].containsbat = true;
+    console.log('Bat has been placed ' + i)
+  }
 }
 
-placeWumpus();
-
-console.log(wumpusCurrentLocation);
-
-function startGame(): void {
-  mainTextArea.innerHTML = 'Great! What would you like your character to be called? <br> Press "Enter" to continue';
-  userTextInput.classList.toggle('hidden');
-  userTextInput.addEventListener('keypress', (e) => {
-    userName = userTextInput.value; // TODO: Fixa max antal tecken 
-    if (e.key === 'Enter') {
-      userTextInput.value = '';
-      userTextInput.classList.toggle('hidden');
-      currentInstance++;
-      nextInstance();
-    }
-  });
-};
+function cavesPlaceEverything(): void {
+  placeWumpus();
+  placeTraps();
+  placeBats();
+}
 
 function nextInstance(): void {
   if (currentInstance == 1) {
@@ -83,6 +95,23 @@ function nextInstance(): void {
     castle of Greveholm. 
     Afraid to be alone? Lucky for you, you are not. There is also a beast by the name of Wumpus in the
     treturous cave system. Your goal is the slay Wumpus before he kills you. There is loot you can find along 
-    the way to aid you, if you need it. Currently you only have a bow and two arrows.`;
+    the way to aid you, if you find it. Currently you only have a bow and two arrows.`;
   }
 }
+
+function startGame(): void {
+  mainTextArea.innerHTML = 'Great! What would you like your character to be called? <br> Press "Enter" to continue';
+  userTextInput.classList.toggle('hidden');
+  userTextInput.addEventListener('keypress', e => {
+    userName = userTextInput.value; // TODO: Fixa max antal tecken
+    if (e.key === 'Enter') {
+      userTextInput.value = '';
+      userTextInput.classList.toggle('hidden');
+      currentInstance += 1;
+      nextInstance();
+    }
+  });
+}
+cavesPlaceEverything();
+
+console.table(allCaves);
